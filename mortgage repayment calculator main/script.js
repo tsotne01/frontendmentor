b7e6888810c6ae16fe2ed9e92baab41110c3954d
tsotne01 <tsotnechkhenkeli@gmail.com>
2024-12-11 22:56:58 +0400

    add see result card and fixed form default reload behaviour

--- mortgage repayment calculator main/script.js
+++ mortgage repayment calculator main/script.js
@@ -7,6 +7,9 @@ const interestRate = document.querySelector("#interest-rate");
 const replacement = document.querySelector("#repayment");
 const interestOnly = document.querySelector("#interest-only");
 
+const seeResultCard = document.querySelector(".see-result");
+const resultCard = document.querySelector(".results");
+
 const monthlyRepayments = document.querySelector(
   ".result__calculation-box-payment-amnt"
 );
@@ -18,16 +21,30 @@ const calcAndDisplayMortgage = (e) => {
   // console.log(e.target);
   // console.log(replacement.checked);
   // console.log(interestOnly.checked);
-
+  e.preventDefault();
   const mrtamount = mortageAmount.value;
   const mrtTrm = mortageTerm.value;
   const intRT = interestRate.value;
   if (replacement.checked) {
+    if (!seeResultCard.classList.contains("hidden")) {
+      seeResultCard.classList.add("hidden");
+    }
+    if (resultCard.classList.contains("hidden")) {
+      resultCard.classList.remove("hidden");
+    }
+
     monthlyRepayments.style.fontSize = "15px";
     totalRepay.style.fontSize = "15px";
     monthlyRepayments.textContent = `calculation results when choosen replacement`;
     totalRepay.textContent = ` calculation results when choosen replacement`;
   } else if (interestOnly.checked) {
+    if (!seeResultCard.classList.contains("hidden")) {
+      seeResultCard.classList.add("hidden");
+    }
+    if (resultCard.classList.contains("hidden")) {
+      resultCard.classList.remove("hidden");
+    }
+
     monthlyRepayments.style.fontSize = "15px";
     totalRepay.style.fontSize = "15px";
     monthlyRepayments.textContent = `calculation results when choosen interest only`;
@@ -42,6 +59,12 @@ const clearEverything = () => {
   interestRate.value = "";
   replacement.checked = false;
   interestOnly.checked = false;
+  if (seeResultCard.classList.contains("hidden")) {
+    seeResultCard.classList.remove("hidden");
+  }
+  if (!resultCard.classList.contains("hidden")) {
+    resultCard.classList.add("hidden");
+  }
 };
 clearAllBtn.addEventListener("click", clearEverything);
 calculateBtn.addEventListener("click", calcAndDisplayMortgage);
